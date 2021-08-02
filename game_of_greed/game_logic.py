53218cc63d9b94b01981b372eda2601c12fbd252
abdu-zeyad <abd.d@live.com>
2021-08-02 16:49:44 +0300

    s

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -3,16 +3,17 @@ from itertools import count
 from random import randint, sample, random
 
 class GameLogic():
-    
+    numberOfDice=6
+    bank=0
 
     def __init__(self):
         pass 
     
-    def calculate_score(test_input):
+    def calculate_score(roll_dice):
         score = 0
         pairs_double =0 
         pairs_trible=0
-        num = collections.Counter(test_input)
+        num = collections.Counter(roll_dice)
         
         """ myList = [1,1,2,3,4,5,3,2,3,4,2,1,2,3]
           Counter({2: '4', 3: '4', 1: '3', 4: '2', 5: '1'})
@@ -24,7 +25,7 @@ class GameLogic():
             num[i]=[3, 4, 4, 2, 1]
         """
 
-        if 1 in num and 2 in num and 3 in num and 4 in num and 5 in num and 6 in num:
+        if len(num) == 6 :
             
             score+=1500
             return score
@@ -59,22 +60,63 @@ class GameLogic():
         print(num)
         return score
 
-    def greeting(self):
-            play = input('do you want to play ? yes,or no?  ')
-            if play == 'yes':
-                print('lets do it ')
-            elif play == 'no':
-                print('we will play later')
+    # def greeting(self):
+    #         play = input('do you want to play ? yes,or no?  ')
+    #         if play == 'yes':
+    #             print('lets do it ')
+    #         elif play == 'no':
+    #             print('we will play later')
             
+    
+
+    def roll_dice(self):
+        roll = tuple(randint(1,6) for _ in range(0, self.numberOfDice))
+        return roll
+
+    def keeper(roll_dice):
+        keeper=[]
+        for x in roll_dice:
+            pick = input(f'type yes if you want to keep  {x}  ')
+            if pick == 'yes':
+                keeper.append(x)
+
+        return keeper
+    
+    def number_dice(self,keeper,roll_dice):
+        self.numberOfDice = -len(roll_dice)+len(keeper)
         
-    def roll_dice(times=6):
-        return tuple(randint(1,6) for _ in range(0, times))
-    #  return sample(range(1, 6 + 1), times)
+        
+class Banker:
+
+    def __init__(self):
+        self.shelved = 0
+        self.balance= 0
+ 
+    def shelf(self,num):
+        self.shelved += num
+   
+    def bank(self):
+        self.balance += self.shelved
+        self.shelved = 0
+        return self.balance
+
+    def clear_shelf(self):
+        self.shelved = 0
 
 
 if __name__ == "__main__":
-    new_game = GameLogic.calculate_score((1,4,5,6))
-    print(new_game)
-    new_game = GameLogic()
-    new_game.greeting()    
-   
+    x = GameLogic.roll_dice(GameLogic)
+    print(x)
+    y = GameLogic.keeper(x)
+    game = GameLogic.calculate_score(y)
+    print(y)
+    print(game)
+
+
+    x = GameLogic.roll_dice(GameLogic)
+    print(x)
+    y = GameLogic.keeper(x)
+    game = GameLogic.calculate_score(y)
+    print(y)
+    print(game)
+
