b24d7a3e3e9304f6ee69aaffc1f284c59addafc1
abdu-zeyad <abd.d@live.com>
2021-08-02 13:25:54 +0300

    add

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -10,16 +10,18 @@ class GameLogic():
     
     def calculate_score(test_input):
         score = 0
-        pairs =0 
+        pairs_double =0 
+        pairs_trible=0
         num = collections.Counter(test_input)
         
         """ myList = [1,1,2,3,4,5,3,2,3,4,2,1,2,3]
-          Counter({2: 4, 3: 4, 1: 3, 4: 2, 5: 1})
+          Counter({2: '4', 3: '4', 1: '3', 4: '2', 5: '1'})
+    
             >>> print Counter(myList).keys()
-            [1, 2, 3, 4, 5]
+            i=[1, 2, 3, 4, 5]
             >>> 
             >>> print Counter(myList).values()
-            [3, 4, 4, 2, 1]
+            num[i]=[3, 4, 4, 2, 1]
         """
 
         if 1 in num and 2 in num and 3 in num and 4 in num and 5 in num and 6 in num:
@@ -27,59 +29,46 @@ class GameLogic():
             score+=1500
             return score
         
-        if (num[1] == 1) and 5 in num:
-            score+=150
-            return score
-
-        if (num[1] == 3) and 5 in num:
-            score+=1050
-            return score
+    
 
         for i in num:
-            
 
             if num[i] == 2:
-                pairs = pairs + 1 
-                if pairs == 3 :
-                    score = 0
-                    score = score +1500
-                    return score
-            
+                pairs_double += 1
+                if pairs_double == 3:
+                    score+=1500
+
+            if num[i] == 3  :
+                pairs_trible+=1
+                if pairs_trible==3:
+                    score+=1200
+                
             if i == 5 and num[i] < 3:
                 score = score + (num[i]*50)
-                return score
 
 
             if i == 1 and num[i] < 3:
                 score = score + (num[i]*100)
-                return score
              
             if num[i] >= 3 and i == 1:
-                score = score + 1000
-                num[i] = num[i] - 3
-                for i in range (num[i]):
-                    score = score + 1000
-                    continue
+                score = score + 1000*(num[i]-2)
+
+            if num[i] >= 3   and i != 1:
+                score = score + ((i)*100*(num[i]-2))
+
+
+
             
-            if num[i] == 3   and i != 1:
-                score = score + (i*100)
-                num[i] = num[i] - 3
-                for i in range (num[i]):
-                    score = score + 100
-                    continue
-            
-            if num[i] > 3   and i != 1:
-                score = score + ((num[i]-2)*i*100)
-                return score
-                continue
+        print(num)
         return score
 
+        
     def roll_dice(times=6):
         return tuple(randint(1,6) for _ in range(0, times))
     #  return sample(range(1, 6 + 1), times)
 
 
 if __name__ == "__main__":
-    new_game = GameLogic()
-
+    new_game = GameLogic.calculate_score((1, 1, 1, 2, 2, 2))
     print(new_game)
+   
