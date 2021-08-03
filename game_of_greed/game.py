444b7fcfdc5e199327b78e39901eb0a2482526bd
abdu-zeyad <abd.d@live.com>
2021-08-03 10:58:39 +0300

    sad

--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -20,7 +20,7 @@ class Game:
         self.num_rounds = num_rounds
 
         self._print("""Welcome to Game of Greed
-(y)es to play or (n)o to decline""")
+        (y)es to play or (n)o to decline""")
 
         response = self._input('>')
 
@@ -29,9 +29,6 @@ class Game:
         elif response == 'n':
             self._print('OK. Maybe later')
 
-
-
-
     def _start(self):
 
         self.score = 0
@@ -39,6 +36,8 @@ class Game:
         round_num = 1
 
         for i in range(1, self.num_rounds + 1):
+            self._print(f'Starting round {round_num}')
+
             round_score = self._do_round()
 
             self._print(f'You banked {round_score} points in round {round_num}')
@@ -52,16 +51,14 @@ class Game:
 
         self._print('Thanks for playing!')
 
-
     def _do_round(self):
         num_dice = 6
 
         score = 0
-        print('Starting round 1')
         while(True):
             self._print(f'Rolling {num_dice} dice..')
 
-            roll = self._do_roll(num_dice)
+            roll = self.roll_dice(num_dice)
 
             zilch_check_score = self.calculate_score(roll)
 
@@ -78,16 +75,19 @@ class Game:
 
             score += self.calculate_score(keepers)
 
-            self._print(f'You can bank {score} points or try for more')
+            self._print(f'You have {score} unbanked points and {num_dice} dice remaining')
 
             if num_dice == 0:
                 num_dice = 6
+            self._print('(r)oll again, (b)ank your points or (q)uit:')
+            
+            roll_again_response = self._input('>')
 
-            self._print(f'You have {num_dice} dice remaining')
+            if  roll_again_response == 'r':
+                continue
 
-            roll_again_response = self._input('Roll again? ')
-
-            if not roll_again_response == 'y':
+            if roll_again_response =='q':
+                print('Thanks for playing. You earned 500 points')
                 break
 
 
@@ -97,7 +97,7 @@ class Game:
 
         while True:
 
-            keep_response = self._input('Enter dice to keep: ')
+            keep_response = self._input('Enter dice to keep, or (q)uit:')
 
             keepers = tuple(int(char) for char in keep_response)
 
@@ -107,82 +107,77 @@ class Game:
                 self._print('No way pal')
                 self._print(roll)
 
-
-
-
-
     def validate(self, roll, keepers):
 
         roll_counter = Counter(roll)
         keepers_counter = Counter(keepers)
         return len(keepers_counter - roll_counter) == 0
 
-
-    def calculate_score(self, dice):
-
+    def calculate_score(self,m ):
         score = 0
+        pairs_double =0 
+        pairs_trible=0
+        num = Counter(m)
+        
+        """ myList = [1,1,2,3,4,5,3,2,3,4,2,1,2,3]
+          Counter({2: '4', 3: '4', 1: '3', 4: '2', 5: '1'})
+    
+            >>> print Counter(myList).keys()
+            i=[1, 2, 3, 4, 5]
+            >>> 
+            >>> print Counter(myList).values()
+            num[i]=[3, 4, 4, 2, 1]
+        """
 
-        dice_counter = Counter(dice)
+        if len(num) == 6 :
+            
+            score+=1500
+            return score
+        
+    
 
-        ones_used = False
-        fives_used = False
+        for i in num:
 
-        for key in dice_counter:
+            if num[i] == 2:
+                pairs_double += 1
+                if pairs_double == 3:
+                    score+=1500
 
-            count = dice_counter[key]
+            if num[i] == 3  :
+                pairs_trible+=1
+                if pairs_trible==3:
+                    score+=1200
+                
+            if i == 5 and num[i] < 3:
+                score = score + (num[i]*50)
 
-            if count >= 3:
-                score += key * 100
 
-                if key == 1:
-                    score = 1000
-                    ones_used = True
-                elif key == 5:
-                    fives_used = True
+            if i == 1 and num[i] < 3:
+                score = score + (num[i]*100)
+             
+            if num[i] >= 3 and i == 1:
+                score = score + 1000*(num[i]-2)
 
-            if count >= 4:
-                if key == 1:
-                    score += 1000
-                else:
-                    score += key * 100
+            if num[i] >= 3   and i != 1:
+                score = score + ((i)*100*(num[i]-2))
 
-            if count >= 5:
-                if key == 1:
-                    score += 1000
-                else:
-                    score += key * 100
-
-            if count == 6:
-                if key == 1:
-                    score += 1000
-                else:
-                    score += key * 100
-
-        # must be a straight
-        if len(dice_counter) == 6:
-            score = 1500
-            ones_used = True
-            fives_used = True
-
-        # must be 3 pairs
-        if len(dice_counter) == 3 and dice_counter.most_common()[0][1] == 2:
-            ones_used = True
-            fives_used = True
-            score = 1500
-
-        if not ones_used:
-            score += dice_counter[1] * 100
-
-        if not fives_used:
-            score += dice_counter[5] * 50
-
+        print(num)
         return score
 
-    def _do_roll(self, num_dice):
-        """simulate dice roll for given number of dice"""
-        return [random.randint(1, 6) for _ in range(num_dice)]
+    def roll_dice(self,x):
+        dice=[]
+        for i in range(x):
+            dice.append(random.randint(1,6))
+            roll = tuple(dice)
+        return roll
 
 
 if __name__ == "__main__":
     game = Game()
-    game.play()
+    # game.play()
+    # x =game._start()
+    
+    y =game._do_round()
+
+    # print(x)
+    print(y)
